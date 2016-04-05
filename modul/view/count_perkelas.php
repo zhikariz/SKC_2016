d39b6243ff95f8fff14d1bb15476e23423b42db4
Albert Septiawan <axquired24@gmail.com>
2016-04-05 11:02:57 +0700

    Added files via upload

--- modul/view/count_perkelas.php
+++ modul/view/count_perkelas.php
@@ -3,12 +3,47 @@
     display: none;
   }
 </style>
+<?php 
+  $count_perorangan = 0; // Jumlah Perorangan 
+  $count_beregu     = 0; // Jumlah Beregu
+
+  $kelas            = new Database;
+  $q_perorangan     = $kelas->custom_query("SELECT COUNT(peserta.id_peserta) AS Jml, kelas_all.* 
+                                            FROM peserta INNER JOIN kelas_all 
+                                            ON peserta.id_kelas=kelas_all.id_kelas 
+                                            WHERE kelas_all.isi_kelas NOT LIKE '%Beregu%'
+                                            GROUP BY peserta.id_kelas");
+
+  $q_beregu         = $kelas->custom_query("SELECT COUNT(peserta.id_peserta) AS Jml, COUNT(peserta.id_peserta)/3 AS Jml_Regu, kelas_all.* 
+                                            FROM peserta INNER JOIN kelas_all 
+                                            ON peserta.id_kelas=kelas_all.id_kelas 
+                                            WHERE kelas_all.isi_kelas LIKE '%Beregu%'
+                                            GROUP BY peserta.id_kelas");
+  
+  foreach ($q_perorangan as $perorangan) {
+    $count_perorangan += $perorangan->Jml;
+  }
+
+  foreach ($q_beregu as $beregu) {
+    $count_beregu += $beregu->Jml_Regu;
+  }
+
+  // Jumlah Semua
+  $count_semua    = $count_beregu + $count_perorangan;
+
+?>
    <!-- Page Content -->
 <div class="container">
     <!-- Heading Row -->
     <div class="row row-centered">
             <div class="row row-centered">
-        <h1> <span class="label label-danger">Jumlah Peserta > Kelas</span> <br><small>Kelas yang dipertandingkan</small></h1>        
+        <h1> <span class="label label-danger">Jumlah Peserta > Kelas</span> <br>
+              <small>              
+              </small>
+        </h1>        
+        <br>  Jumlah Pertandingan : <strong><?php echo $count_semua; ?></strong> <br>
+              Detail : <?php echo " Jumlah Perorangan : $count_perorangan // Jumlah Regu (Kata Beregu) : $count_beregu"; ?>
+        <br>              
         <div class="col-md-12">
             <table id="perkelas3" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
