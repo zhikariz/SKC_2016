a33cb3e2e154995e6208129403871e16970c4fe2
Albert Septiawan <axquired24@gmail.com>
2016-04-07 14:38:51 +0700

    Added files via upload

---
+++ lib/aio_config.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+// All In One Config
+$host 		= "localhost";
+$user_sql	= "root";
+$pass_sql 	= "root";
+$dbn  		= "skc_solocup";
+
+//PDO Method Connection
+ini_set( "display_errors", true );
+define( "DB_DSN", "mysql:host=".$host.";dbname=".$dbn);
+define( "DB_USERNAME", $user_sql );
+define( "DB_PASSWORD", $pass_sql );
+define('DB_CHARACSET', 'utf8');
+
+// Used for Old login Method
+$koneksi_login 	= mysql_connect($host, $user_sql, $pass_sql) or die("Konesi Ke Server DB Bermasalah"); 
+$dbname 		= mysql_select_db($dbn) or die("Database tidak tersedia / down"); 
+?>
--- lib/config.php
+++ lib/config.php
@@ -1,11 +1,5 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-ini_set( "display_errors", true );
-define( "DB_DSN", "mysql:host=localhost;dbname=skc_solocup" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "root" );
-define('DB_CHARACSET', 'utf8');
-
+require_once('lib/aio_config.php');
 require_once ('lib/Database.php');
 require_once ('lib/Dtable.php');
 
