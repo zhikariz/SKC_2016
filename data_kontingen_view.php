b0b6a0b8c97d91814eace0d4ecafc69c4b64f893
Albert Septiawan <axquired24@gmail.com>
2016-04-08 10:52:32 +0700

    Added files via upload

--- data_kontingen_view.php
+++ data_kontingen_view.php
@@ -4,25 +4,24 @@ include "modul/enkripsi/function.php";
 //kolom apa saja yang akan ditampilkan
 $columns = array(
 	'id_kontingen',
-	'isi_kontingen'
+	'isi_kontingen',
+	'nama_official',
+	'kontak_official'
 	);
-
 //lakukan query data dari 3 table dengan inner join
 	$query = $datatable->get_custom("SELECT * FROM kontingen_all",$columns);
-
 	//buat inisialisasi array data
 	$data = array();
-
 	foreach ($query	as $value) {
-
 	//array sementara data
 	$ResultData = array();
 	//masukan data ke array sesuai kolom table
 	$ResultData[] = $value->id_kontingen;
 	$ResultData[] = $value->isi_kontingen;
+	$ResultData[] = $value->nama_official;
+	$ResultData[] = $value->kontak_official;
 
 	//bisa juga pake logic misal jika value tertentu maka outputnya
-
 	//kita bisa buat tombol untuk keperluan edit, delete, dll, 
 	$edit = paramEncrypt("uri=kontingen/kontingen_edit&id_kontingen=".$value->id_kontingen);
 	$del = paramEncrypt("uri=kontingen/kontingen_delete&id_kontingen=".$value->id_kontingen);
@@ -30,18 +29,14 @@ $columns = array(
 						"edit" 		=> $edit,
 						"delete"	=> $del,
 						"del_msg"	=> "confirm return('Hapus Data ".$value->isi."? ')"
-
 					);
 	$ResultData[] = "
 		<a title='Ubah Data' href='./?".$crud_uri[edit]."' class='link'><span class='glyphicon glyphicon-pencil btn btn-xs btn-default'></span></a>
 		<a title='Hapus Data' onclick=confirmHapus('./?".$crud_uri[delete]."') href='#' class='link'><span class='glyphicon glyphicon-trash btn btn-xs btn-danger'></span></a>
 	";
-
 	//memasukan array ke variable $data
-
 	$data[] = $ResultData;
 }
-
 //set data
 $datatable->set_data($data);
 //create our json
